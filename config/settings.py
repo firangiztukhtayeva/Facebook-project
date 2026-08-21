332bbba1d293b54252abd8dccc7f66a447a7ed32
Ferangiz <firangiztukhtayeva@gmail.com>
2026-08-21 09:09:09 +0500

    static fayllar ulandi

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
