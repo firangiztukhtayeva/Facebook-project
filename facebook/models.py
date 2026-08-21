2395b4dd28505241ce9b1c897730ae9ba7530b48
Ferangiz <firangiztukhtayeva@gmail.com>
2026-08-21 10:04:02 +0500

    post modeli yaratatildi

--- facebook/models.py
+++ facebook/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='comments/')
+    text = models.TextField()
+    created_at = models.DateTimeField()
