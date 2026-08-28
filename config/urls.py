92824cb18ea51d584537b078dce0a1a97ecf04b0
Ferangiz <firangiztukhtayeva@gmail.com>
2026-08-28 06:11:27 +0500

    config urls.py ga accounts urls ulandi

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('facebook.urls'))
+    path('',include('facebook.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
